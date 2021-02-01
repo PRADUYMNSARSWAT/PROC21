cf7659aeea2a190be0bfdca7cb07c60734544181
PRADUYMNSARSWAT <77499690+PRADUYMNSARSWAT@users.noreply.github.com>
2021-02-01 21:05:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  car = createSprite(100,200,80,30);
+  car.shapeColor = "blue";
+  car.debug = true;
+  car.velocityX= 5;
+
+  wall = createSprite(750,200,80,700);
+  wall.shapeColor = "gray";
+  wall.debug = true;
+  
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+    if(   isTouching(car,wall)===true){
+      car.shapeColor = "green";
+    wall.shapeColor = "green";
+    }
+    else{
+    car.shapeColor = "red";
+      wall.shapeColor = "red";
+
+    }
+  drawSprites();
+}
+
+
+
+
+
+
+
+
